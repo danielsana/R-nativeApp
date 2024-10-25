3482ece63ec954337e1dfe46459acfd57e7aa29d
dansana <danielsana60@gmail.com>
2024-10-25 06:08:37 +0300

    nativeapp

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image ,TouchableOpacity, Dimensions, Platform} from 'react-native';
+import { useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 
 export default function App() {
+  const {landscape} = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "baseline",
+      }}>
+        <View style={{
+        backgroundColor: "dodgerblue",
+        width: 100,
+        height: 300,
+      }}>
+        
+      </View>
+
+      <View style={{
+        backgroundColor: "gold",
+        width: 100,
+        height: 200,
+      }}>
+        
+      </View>
+
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 100,
+      }}>
+        
+      </View>
+        
+      </View>
+      
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff9900',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ app/screens/WelcomScreen.js
@@ -0,0 +1,14 @@
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function WelcomScreen() {
+  return (
+    <ImageBackground>
+        
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
